8646b3490a1fdd481b284204a18c25ebf6e982f6
DavidTheFighter <19dallen@gmail.com>
2025-03-02 22:04:06 -0800

    Added arctic theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'DavidTheFighter/arctic.nvim',
+
+    dependencies = { 'rktjmp/lush.nvim' },
+    name = 'arctic',
+    branch = 'v2',
+    priority = 999,
+    config = function()
+      vim.cmd 'colorscheme arctic'
+    end,
+  },
+}
