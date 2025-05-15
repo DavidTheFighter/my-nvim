b2e33c16bb7f96a71d37cd0bc081aab711690454
David Allen <allen@astranis.com>
2025-05-15 10:42:27 -0700

    Updated work plugin lua

--- lua/work.lua
+++ lua/work.lua
@@ -1,5 +1,43 @@
-local M = {
-  local_plugin_directory = 'work_plugins',
-}
+local M = {}
+
+function M.codecompanion_strategies()
+  return {
+    chat = {
+      adapter = 'bedrock_claude_chat',
+    },
+    inline = {
+      adapter = 'bedrock_claude_chat',
+    },
+    cmd = {
+      adapter = 'bedrock_claude_chat',
+    },
+  }
+end
+
+function M.codecompanion_adapters()
+  return {
+    opts = {
+      show_defaults = false,
+    },
+    bedrock_claude_chat = function()
+      return require('codecompanion.adapters').extend('openai_compatible', {
+        env = {
+          url = 'http://172.31.25.243:8000',
+        },
+        schema = {
+          messages = {
+            { role = 'user', content = '' },
+          },
+          model = {
+            default = 'bedrock-claude-v1',
+          },
+          max_tokens = {
+            default = 200000,
+          },
+        },
+      })
+    end,
+  }
+end
 
 return M
